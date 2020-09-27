f367ab0fd48043df149ed07ad326cf3e9312917a
nada kamal <mlak.eldonia99.me@gmail.com>
2020-09-27 23:14:06 +0200

    Revert "drag to delete"
    This reverts commit 09cd37789a6bb5971283be214b71361e6ba6ed1f.

--- app/src/main/java/com/example/project/Adapter.java
+++ app/src/main/java/com/example/project/Adapter.java
@@ -10,31 +10,33 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements ItemTouchHelperAdapter, View.OnLongClickListener {
-    public Adapter(ArrayList<ItemModel> items) {
+    public Adapter(ArrayList<ItemModel> items)
+    {
         this.items = items;
-        // this.onProductClickListener = onProductClickListener;
-    }
+       // this.onProductClickListener = onProductClickListener;
+        }
 
     ItemTouchHelper mTouchHelper;
-    //  OnProductClickListener onProductClickListener;
-    static int id = 0;
-    static String name = null;
+   //  OnProductClickListener onProductClickListener;
+  static int id = 0;
+  static String name = null;
     ArrayList<ItemModel> items = new ArrayList();
-
-    // private onClickItemListener itemListener;
+   // private onClickItemListener itemListener;
     @Override
     public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
-        return new holder(view);
-    }
+        return new holder(view); }
 
     @Override
     public void onBindViewHolder(@NonNull final holder holder, final int position) {
@@ -44,7 +46,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         holder.itemView.setOnLongClickListener(this);
     }
 
-    @Override
+        @Override
     public int getItemCount() {
         return items.size();
     }
@@ -73,14 +75,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         return true;
     }
 
-    public void emptyText(int position) {
-        items.set(position, new ItemModel(""));
-        notifyItemChanged(position);
-    }
 
-
-    public class holder extends RecyclerView.ViewHolder implements View.OnTouchListener, GestureDetector.OnGestureListener {
-        ImageView itemImage;
+    public class holder extends RecyclerView.ViewHolder implements View.OnTouchListener, GestureDetector.OnGestureListener
+   {
+       ImageView itemImage;
         ImageView bg;
         TextView itemText;
         GestureDetector gestureDetector;
@@ -90,9 +88,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
             bg = itemView.findViewById(R.id.content);
             itemImage = itemView.findViewById(R.id.item_image);
             itemText = itemView.findViewById(R.id.tv_content);
-//            gestureDetector = new GestureDetector(itemView.getContext(), this);
-//            itemView.setOnTouchListener(this);
-            itemView.setOnLongClickListener(new MainActivity.MyDragShadowBuilder.MyLongClickListener());
+            gestureDetector = new GestureDetector(itemView.getContext(), this);
+            itemView.setOnTouchListener(this);
+
         }
 
         @Override
@@ -109,7 +107,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         @Override
         public boolean onSingleTapUp(MotionEvent e) {
             id = getAdapterPosition();
-            name = items.get(id).getItemName();
+            name =  items.get(id).getItemName();
             return false;
         }
 
@@ -121,7 +119,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         @Override
         public void onLongPress(MotionEvent e) {
             mTouchHelper.startDrag(this);
-            //  v.startDrag(ClipData.newPlainText("label", v.getTag().toString()), new View.DragShadowBuilder(v), null, 0);
+          //  v.startDrag(ClipData.newPlainText("label", v.getTag().toString()), new View.DragShadowBuilder(v), null, 0);
 
         }
 
@@ -136,6 +134,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
             gestureDetector.onTouchEvent(event);
             return true;
         }
+
 
 
     }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,24 +1,5 @@
 package com.example.project;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
-import android.content.res.Configuration;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.DragEvent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -30,32 +11,45 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Configuration;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import com.google.android.material.navigation.NavigationView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnDragListener {
+public class MainActivity extends AppCompatActivity implements View.OnDragListener
+
+{
+    private ListView mDrawerList ;
     RecyclerView items;
     ImageView deleteImage;
+    private DrawerLayout mDrawerLayout;
     NavigationView navigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
     ArrayList dataList = new ArrayList();
     String selectedItem;
     ArrayList arrayList = new ArrayList();
-    ItemModel item;
-    int itemId;
+   ItemModel item;
+    int itemId ;
     CoordinatorLayout coordinatorLayout;
-    String gridItem;
+    String gridItem ;
     TextView tvSaveChanges;
     Toolbar toolbar;
-    File gridFile, listFile;
-    static Adapter adapter;
-    private ListView mDrawerList;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private MyDragShadowBuilder.MyDragEventListener mDragListener;
-    private static final String TAG = "MainActivity";
-
+    File gridFile,listFile;
+    Adapter adapter;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,91 +69,89 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open,
-                R.string.drawer_close);
+          mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.drawer_open,
+        R.string.drawer_close);
 
 
-        mDragListener = new MyDragShadowBuilder.MyDragEventListener();
-
-
-        deleteImage.setOnDragListener(mDragListener);
-
-        // coordinatorLayout.setOnDragListener(this);
+         // coordinatorLayout.setOnDragListener(this);
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         items = findViewById(R.id.ContentGridView);
 
-        dataList.add("4");
-        dataList.add("5");
-        dataList.add("6");
+         dataList.add("4");
+         dataList.add("5");
+         dataList.add("6");
 
-        tvSaveChanges.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+tvSaveChanges.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
 
-            }
-        });
-        final NavigationAdapter adapter1 = new NavigationAdapter(this, 0, dataList);
-        // mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_item,dataList));
+    }
+});
+final NavigationAdapter adapter1 = new NavigationAdapter(this,0,dataList);
+     // mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_item,dataList));
         mDrawerList.setAdapter(adapter1);
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                int img = 0;
-                selectedItem = (String) dataList.get(position);
+        int img = 0;
+        selectedItem = (String) dataList.get(position);
 
-                if (selectedItem != null) {
-                    switch (selectedItem) {
-                        case "1":
-                            img = R.drawable.ic_baseline_looks_1;
-                            break;
-                        case "2":
-                            img = R.drawable.ic_baseline_looks_2;
-                            break;
-                        case "3":
-                            img = R.drawable.ic_baseline_looks_3;
-                            break;
-                        case "4":
-                            img = R.drawable.ic_baseline_looks_4;
-                            break;
-                        case "5":
-                            img = R.drawable.ic_baseline_looks_5;
-                            break;
-                        case "6":
-                            img = R.drawable.ic_baseline_looks_6;
-                            break;
-                    }
-                    item = new ItemModel(selectedItem, img);
-
-                    arrayList.set(Adapter.id, item);
-                    adapter.notifyDataSetChanged();
-                    if (Adapter.name == "") {
-                        dataList.remove(position);
-                    } else {
-                        dataList.set(position, Adapter.name);
-
-                    }
-                    adapter1.notifyDataSetChanged();
-                    adapter1.notifyDataSetChanged();
-
-
-                }
+      if (selectedItem != null)
+      { switch (selectedItem) {
+                case "1":
+                    img = R.drawable.ic_baseline_looks_1;
+                    break;
+                case "2":
+                    img = R.drawable.ic_baseline_looks_2;
+                    break;
+                case "3":
+                    img = R.drawable.ic_baseline_looks_3;
+                    break;
+                case "4":
+                    img = R.drawable.ic_baseline_looks_4;
+                    break;
+                case "5":
+                    img = R.drawable.ic_baseline_looks_5;
+                    break;
+                case "6":
+                    img = R.drawable.ic_baseline_looks_6;
+                    break;
             }
-        });
+            item = new ItemModel(selectedItem, img);
+
+            arrayList.set(Adapter.id, item);
+            adapter.notifyDataSetChanged();
+          if(Adapter.name == "") {
+              dataList.remove(position);
+          }
+          else
+          {
+          dataList.set(position,Adapter.name);
+
+          }
+            adapter1.notifyDataSetChanged();
+            adapter1.notifyDataSetChanged();
 
 
-        arrayList.add(new ItemModel("1", R.drawable.ic_baseline_looks_1));
+      }
+    }
+});
+
+
+
+        arrayList.add(new ItemModel("1",R.drawable.ic_baseline_looks_1));
         arrayList.add(new ItemModel(""));
         arrayList.add(new ItemModel(""));
-        arrayList.add(new ItemModel("2", R.drawable.ic_baseline_looks_2));
+        arrayList.add(new ItemModel("2",R.drawable.ic_baseline_looks_2));
         arrayList.add(new ItemModel(""));
         arrayList.add(new ItemModel(""));
-        arrayList.add(new ItemModel("3", R.drawable.ic_baseline_looks_3));
+        arrayList.add(new ItemModel("3",R.drawable.ic_baseline_looks_3));
         arrayList.add(new ItemModel(""));
         arrayList.add(new ItemModel(""));
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,3);
         ItemTouchHelper.Callback callback = new TouchHelper(adapter);
         final ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
         adapter.setTouchHelper(itemTouchHelper);
@@ -167,6 +159,8 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
         items.setLayoutManager(layoutManager);
         items.setAdapter(adapter);
     }
+
+
 
 
     @Override
@@ -221,168 +215,11 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
                     v.setY(oldY);
                     return true;
                 }
-            default:
-                return false;
-        }
-    }
-
-    static class MyDragShadowBuilder extends View.DragShadowBuilder {
-        // The drag shadow image, defined as a drawable thing
-        private Drawable shadow;
-
-        public MyDragShadowBuilder(View v) {
-            super(v);
-
-            int color = ((ColorDrawable) v.getBackground()).getColor();
-
-            shadow = new ColorDrawable(getDarkerColor(color));
-        }
-
-        @Override
-        public void onProvideShadowMetrics(Point size, Point touch) {
-            int width, height;
-
-            width = getView().getWidth() / 2;
-
-            height = getView().getHeight() / 2;
-
-            shadow.setBounds(0, 0, width, height);
-
-            size.set(width, height);
-
-            touch.set(width / 2, height / 2);
-        }
-
-        @Override
-        public void onDrawShadow(Canvas canvas) {
-            // Draws the ColorDrawable in the Canvas passed in from the system
-            shadow.draw(canvas);
-        }
-
-        static class MyDragEventListener implements View.OnDragListener {
-
-            public boolean onDrag(View view, DragEvent event) {
-                final int action = event.getAction();
-
-                switch (action) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                            view.setBackgroundColor(Color.parseColor("#FFC4E4FF"));
-                            return true;
-                        }
-                        return false;
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        // When dragged item entered the receiver view area
-                        view.setBackgroundColor(Color.parseColor("#FFB7FFD6"));
-                        return true;
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        // Ignore the event
-                        return true;
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        // When dragged object exit the receiver object
-                        view.setBackgroundColor(Color.parseColor("#FFFFBCBC"));
-                        // Return true to indicate the dragged object exited the receiver view
-                        return true;
-                    case DragEvent.ACTION_DROP:
-                        // Get the dragged data
-                        ClipData.Item item = event.getClipData().getItemAt(0);
-                        String dragData = (String) item.getText();
-                        Log.d(TAG, "onDrag: " + dragData);
-                        try {
-                            adapter.emptyText(Integer.parseInt(dragData));
-                        } catch (Exception e) {
-                            Log.e(TAG, "onDrag: " + e.getMessage());
-                        }
-
-                        return true;
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        // Remove the background color from view
-                        view.setBackgroundColor(Color.TRANSPARENT);
-                        if (event.getResult()) {
-                            Log.d(TAG, "onDrag: The drop was handled.");
-                        } else {
-                            Log.d(TAG, "onDrag: The drop did not work.");
-                        }
-
-                        // Return true to indicate the drag ended
-                        return true;
                     default:
-                        Log.e("Drag and Drop example", "Unknown action type received.");
-                        break;
+                        return false;
                 }
-
-                return false;
-            }
-
-
         }
-
-        public int getDarkerColor(int color) {
-            float[] hsv = new float[3];
-            Color.colorToHSV(color, hsv);
-            //hsv[2] = 0.8f *hsv[2];
-            hsv[2] = 0.7f * hsv[2]; // more darker
-            return Color.HSVToColor(hsv);
-        }
-
-        static class MyLongClickListener implements View.OnLongClickListener {
-            @Override
-            public boolean onLongClick(View view) {
-                // Get the background color of view
-                int color = ((ColorDrawable) view.getBackground()).getColor();
-                String colorString = String.valueOf(color);
-
-            /*
-                ClipData.Item
-                    Description of a single item in a ClippedData.
-                    The types than an individual item can currently contain are: Text, Intent and Uri.
-            */
-                // Create a new ClipData
-                // Create a ne ClipData.Item from the TextView objects tag
-                ClipData.Item item = new ClipData.Item(colorString);
-
-            /*
-                ClipData
-                    Representation of a clipped data on the clipboard.
-
-                    ClippedData is a complex type containing one or Item instances, each of which
-                    can hold one or more representations of an item of data. For display to the
-                    user, it also has a label and iconic representation.
-
-                    A ClipData contains a ClipDescription, which describes important meta-data
-                    about the clip. In particular, its getDescription().getMimeType(int) must
-                    return correct MIME type(s) describing the data in the clip. For help in
-                    correctly constructing a clip with the correct MIME type, use
-                    newPlainText(CharSequence, CharSequence), newUri(ContentResolver,
-                    CharSequence, Uri), and newIntent(CharSequence, Intent).
-
-                    Each Item instance can be one of three main classes of data: a simple
-                    CharSequence of text, a single Intent object, or a Uri.
-            */
-                // Create a new ClipData using the tag as a label, the plain text MIME type,
-                // and the already created item.
-                ClipData dragData = new ClipData(
-                        colorString,
-                        new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item
-                );
-
-                // Instantiates the drag shadow builder
-                View.DragShadowBuilder myShadow = new MyDragShadowBuilder(view);
-
-                // Starts the drag
-                view.startDrag(
-                        dragData, //  the data to be drag
-                        myShadow, // the drag shadow builder
-                        null, // no need to use local data
-                        0 // flags
-                );
-                return false;
-            }
-
-        }
-
 
     }
-}
 
 
