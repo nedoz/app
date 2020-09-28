5c0ff166ae989efc03901762d6b4b7af8537f940
nada kamal <mlak.eldonia99.me@gmail.com>
2020-09-28 12:00:12 +0200

    Revert "Revert "drag to delete""
    This reverts commit f367ab0fd48043df149ed07ad326cf3e9312917a.

--- app/src/main/java/com/example/project/Adapter.java
+++ app/src/main/java/com/example/project/Adapter.java
@@ -10,33 +10,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements ItemTouchHelperAdapter, View.OnLongClickListener {
-    public Adapter(ArrayList<ItemModel> items)
-    {
+    public Adapter(ArrayList<ItemModel> items) {
         this.items = items;
-       // this.onProductClickListener = onProductClickListener;
-        }
+        // this.onProductClickListener = onProductClickListener;
+    }
 
     ItemTouchHelper mTouchHelper;
-   //  OnProductClickListener onProductClickListener;
-  static int id = 0;
-  static String name = null;
+    //  OnProductClickListener onProductClickListener;
+    static int id = 0;
+    static String name = null;
     ArrayList<ItemModel> items = new ArrayList();
-   // private onClickItemListener itemListener;
+
+    // private onClickItemListener itemListener;
     @Override
     public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
-        return new holder(view); }
+        return new holder(view);
+    }
 
     @Override
     public void onBindViewHolder(@NonNull final holder holder, final int position) {
@@ -46,7 +44,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         holder.itemView.setOnLongClickListener(this);
     }
 
-        @Override
+    @Override
     public int getItemCount() {
         return items.size();
     }
@@ -75,10 +73,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         return true;
     }
 
+    public void emptyText(int position) {
+        items.set(position, new ItemModel(""));
+        notifyItemChanged(position);
+    }
 
-    public class holder extends RecyclerView.ViewHolder implements View.OnTouchListener, GestureDetector.OnGestureListener
-   {
-       ImageView itemImage;
+
+    public class holder extends RecyclerView.ViewHolder implements View.OnTouchListener, GestureDetector.OnGestureListener {
+        ImageView itemImage;
         ImageView bg;
         TextView itemText;
         GestureDetector gestureDetector;
@@ -88,9 +90,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
             bg = itemView.findViewById(R.id.content);
             itemImage = itemView.findViewById(R.id.item_image);
             itemText = itemView.findViewById(R.id.tv_content);
-            gestureDetector = new GestureDetector(itemView.getContext(), this);
-            itemView.setOnTouchListener(this);
-
+//            gestureDetector = new GestureDetector(itemView.getContext(), this);
+//            itemView.setOnTouchListener(this);
+            itemView.setOnLongClickListener(new MainActivity.MyDragShadowBuilder.MyLongClickListener());
         }
 
         @Override
@@ -107,7 +109,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         @Override
         public boolean onSingleTapUp(MotionEvent e) {
             id = getAdapterPosition();
-            name =  items.get(id).getItemName();
+            name = items.get(id).getItemName();
             return false;
         }
 
@@ -119,7 +121,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
         @Override
         public void onLongPress(MotionEvent e) {
             mTouchHelper.startDrag(this);
-          //  v.startDrag(ClipData.newPlainText("label", v.getTag().toString()), new View.DragShadowBuilder(v), null, 0);
+            //  v.startDrag(ClipData.newPlainText("label", v.getTag().toString()), new View.DragShadowBuilder(v), null, 0);
 
         }
 
@@ -134,7 +136,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.holder> implements Ite
             gestureDetector.onTouchEvent(event);
             return true;
         }
-
 
 
     }
